e9c78639a79a15a2caf4db6481c8c7c629d36422
Anton Shanaurin <grspectre@gmail.com>
2019-01-02 12:35:22 +0300

    readme and license

---
+++ README.md
@@ -0,0 +1,55 @@
+# JPGTODATEDDIR 1.0.0
+
+Console utility for copying jpg and tiff images to dated folders in source.
+
+### Prerequisites
+
+* Python 3
+* virtualenv
+* ExifRead
+
+### Installing
+
+On working directory:
+
+```
+# linux
+virtualenv --clear -p python3 venv
+# win
+virtualenv --clear venv
+
+# linux
+source venv/bin/activate
+# win
+venv\Scripts\activate.bat
+
+pip install -r requirments.txt
+```
+
+### Usage
+
+```
+usage: jpgtodir.py [-h] [-p POSTFIX] source_dir
+
+Move JPG and TIFF images to dated directory
+
+positional arguments:
+  source_dir            Source directory
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -p POSTFIX, --postfix POSTFIX
+                        Postfix for dated directory
+```
+
+## Versioning
+
+We use [SemVer](http://semver.org/) for versioning. For the versions available, see the [tags on this repository](https://github.com/your/project/tags). 
+
+## Authors
+
+* **Anton Shanaurin** - *Initial work* - [grspectre](https://github.com/grspectre)
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
--- jpgtodir.py
+++ jpgtodir.py
@@ -11,7 +11,7 @@ import shutil
 
 def main():
     allowed_file_types = ['jpeg', 'tiff']
-    path = get_path()
+    path, postfix = get_params()
     # curious bug with path in "" (win10)
     if path[-1] == '"':
         path = path[:-1]
@@ -24,7 +24,7 @@ def main():
                 file_path = os.path.join(path, entry.name)
                 file_type = imghdr.what(file_path)
                 if file_type in allowed_file_types:
-                    part_path = process_file(file_path)
+                    part_path = process_file(file_path, postfix)
                     new_path = os.path.join(path, part_path)
                     new_path = os.path.normpath(new_path)
                     if not os.path.isdir(new_path):
@@ -34,10 +34,12 @@ def main():
                         shutil.move(file_path, new_file)
 
 
-def process_file(img_path):
+def process_file(img_path, postfix):
     time_format = '%Y:%m:%d %H:%M:%S'
-    path_format = '%Y/%m/%d'
+    path_format = '%Y/%m/%Y%m%d'
     default_path = 'unsorted'
+    if postfix is not None:
+        default_path += ' ' + postfix
     date_tag_name = 'EXIF DateTimeOriginal'
     f = open(img_path, 'rb')
     tags = exifread.process_file(f, stop_tag=date_tag_name)
@@ -46,14 +48,18 @@ def process_file(img_path):
         return default_path
     img_datetime = datetime.datetime.strptime(str(tags[date_tag_name]), time_format)
     f.close()
-    return img_datetime.strftime(path_format)
+    output_path = img_datetime.strftime(path_format)
+    if postfix is not None:
+    	output_path += ' ' + postfix
+    return output_path
 
 
-def get_path():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('path')
+def get_params():
+    parser = argparse.ArgumentParser(description="Move JPG and TIFF images to dated directory")
+    parser.add_argument('source_dir', help="Source directory")
+    parser.add_argument('-p', '--postfix', type=str, help="Postfix for dated directory")
     args = parser.parse_args()
-    return args.path
+    return [args.source_dir, args.postfix]
 
 
 def print_error(message):
